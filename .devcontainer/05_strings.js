c08a9e3eb4d304401088c2e3c3417cfbfab8eaa4
yashikayadav9550 <jogi161182@gmail.com>
2026-07-16 15:40:43 +0000

    change in file position

---
+++ .devcontainer/05_strings.js
@@ -0,0 +1,15 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// string interpolation
+
+const Name = "dhdhfkhfgh"
+
+console.log(Name[0]);
+console.log(Name.__proto__);
+
+
--- .devcontainer/datatypes-summary.js
+++ .devcontainer/datatypes-summary.js
@@ -1,4 +1,6 @@
-// Primitive
+
+
+-// Primitive
 
 // 7 types : String , Number , Boolean  ,Null , Undefined , BigInt , Symbol
 
